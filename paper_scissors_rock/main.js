eadb896d3ef1256e70193cd11e7a0a75a5625f7d
jayHeo <jihoon.heo@gmail.com>
2019-01-19 14:53:07 -0800

    finished

--- paper_scissors_rock/main.js
+++ paper_scissors_rock/main.js
@@ -1,48 +1,48 @@
-let yourHand = prompt('choose rock paper scissor');
+function start(){
+    let yourHand = prompt('choose rock paper scissor');
+    let computer = game(Math.random() * 100 * 3);
 
-let computer = game(Math.random() * 100 * 3);
+    function game(number){
+        if (number < 100){
+            return 'rock'
+        }else if(number >= 100 && number <= 200){
+            return 'scissor'
+        }else{
+            return 'paper'
+        };
+    };
 
-function game(number){
-  if (number < 100){
-    return 'rock'
-  }else if(number >= 100 && number <= 200){
-    return 'scissor'
-  }else{
-    return 'paper'
-  };
-}
-
-function match() {
-  if(yourHand === computer){
-    return 'tie';
-  };
-  
-  if(yourHand === 'rock'){
-    if (computer === 'paper'){
-      return 'computer won'
-    } else {
-      return 'you won'
+    function match() {
+        if(yourHand === computer){
+            return 'tie';
+        }; 
+        if(yourHand === 'rock'){
+            if (computer === 'paper'){
+                return 'computer won'
+            } else {
+                return 'you won'
+            }
+        };
+        if(yourHand === 'scissor'){
+            if (computer === 'paper'){
+                return 'you won'
+            } else {
+                return 'computer won'
+            }
+        };
+        if(yourHand === 'paper'){
+            if (computer === 'scissor'){
+                return 'computer won'
+            }else {
+                return 'you won'
+            }
+        }
     }
-  }
-  if(yourHand === 'scissor'){
-    if (computer === 'paper'){
-      return 'you won'
-    } else {
-      return 'computer won'
-    }
-  }
-  
-  if(yourHand === 'paper'){
-    if (computer === 'scissor'){
-      return 'computer won'
-    } else {
-      return 'you won'
-    }
-  }
-}
 
 document.write('your hand is: ' + yourHand);
 document.write("<br>");
 document.write('computer hand is: '+  computer);
 document.write("<br>");
 document.write('result: ' + match());
+
+}
